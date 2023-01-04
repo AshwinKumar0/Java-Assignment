71f3d0d6b47df88f517157abfc282c63d96bf5b5
mrkool <ak1380042@gmail.com>
2023-01-05 00:25:14 +0530

    stack updates

--- mrkool/DSA/stack.java
+++ mrkool/DSA/stack.java
@@ -1,2 +1,19 @@
-package mrkool;public class stack {
+package mrkool;
+import java.util.Stack;
+public class stack {
+    public static void main(String[] args) {
+        Stack<Integer> myStack = new Stack<>();
+        myStack.push(10);
+        myStack.push(15);
+        myStack.push(20);
+        myStack.push(25);
+
+        int val = myStack.pop();
+        System.out.println(val);
+        System.out.println(myStack.peek());
+        System.out.println(myStack.isEmpty());
+        System.out.println(myStack.search(25));
+
+    }
+
 }
