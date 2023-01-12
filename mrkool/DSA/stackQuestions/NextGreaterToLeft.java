06fe0f80120ada85d2e3c3d2c40eef151f3a3836
mrkool <ak1380042@gmail.com>
2023-01-12 13:32:29 +0530

    greatertoleft

---
+++ mrkool/DSA/stackQuestions/NextGreaterToLeft.java
@@ -0,0 +1,21 @@
+package mrkool.stackQuestions;
+/*
+"Next greater element on the left" of an element x is defined as the first element to left of x having value greater than x.
+Note -> If an element does not have any element on it's left side greater than it, consider -1 as it's "next greater element on left"
+e.g.
+for the array [2 5 9 3 1 12 6 8 7]
+Next greater for 2 is -1
+Next greater for 5 is -1
+Next greater for 9 is -1
+Next greater for 3 is 9
+Next greater for 1 is 3
+Next greater for 12 is -1
+Next greater for 6 is 12
+Next greater for 8 is 12
+Next greater for 7 is 8
+ */
+public class NextGreaterToLeft {
+    public static void main(String[] args) {
+   
+    }
+}
